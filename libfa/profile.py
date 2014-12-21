2a73d6ce49031d8c3b35621c8a3e0f7d21f918ed
Wes Weber <wweber@wesweber.com>
2014-12-20 22:08:25 -0500

    Watch key

--- libfa/profile.py
+++ libfa/profile.py
@@ -8,7 +8,7 @@ class Profile:
     full_name = None
     watch_key = None
 
-def get_profile_by_login_name(session, login_name):
+def get_by_login_name(session, login_name):
     url = "/user/%s/" % login_name
 
     page = session.perform_request("GET", url)
@@ -20,10 +20,10 @@ def get_profile_by_login_name(session, login_name):
     profile.full_name = selector(page)[0][0].tail.strip()
 
     # Watch key
-    # selector = CSSSelector(".tab > b:nth-child(9) > a:nth-child(1)")
-    # key_url = selector(page)[0].get("href")
+    selector = CSSSelector(".tab > b:nth-child(9) > a:nth-child(1)")
+    key_url = selector(page)[0].get("href")
 
-    # m = re.search("\\?key=(.*)", key_url)
-    # profile.watch_key = key_url.group(1)
+    m = re.search("\\?key=(.*)", key_url)
+    profile.watch_key = m.group(1)
 
     return profile
